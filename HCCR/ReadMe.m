ef4d1ea306d3564f8bab4467eae24d81926e9bfe
shengyudingli <ecnuhjzhan@gmail.com>
2017-10-27 20:01:29 +0800

    Update ReadMe.m

--- HCCR/ReadMe.m
+++ HCCR/ReadMe.m
@@ -2,7 +2,8 @@
 1. DropSample: A new training method to enhance deep convolutional neural networks
    for large-scale unconstrained handwritten Chinese character recognition.  Yang Weixin, Jin Lianwen
 2. ICDAR 2013 Chinese Handwriting Recognition Competition.  Cheng-Lin Liu 
-3. Online and offline handwritten Chinese character recognition: A comprehensive study and new benchmar. Xu-Yao Zhang Cheng-Lin Liu
+3. Online and offline handwritten Chinese character recognition: A comprehensive 
+   study and new benchmar. Xu-Yao Zhang Cheng-Lin Liu
 
 
 
